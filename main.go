d666e29c7a0bdc8f88b7b3e43b783ad4583ab103
pr0jectf1 <luis.diego.garcia.618@gmail.com>
2025-04-29 12:06:12 -0400

    Go - Function Implementations practice

--- main.go
+++ main.go
@@ -1,10 +1,63 @@
 package main
 
-import "fmt"
+func placeOrder(productID string, quantity int, accountBalance float64) (bool, float64) {
+    if stockAmount := amountInStock(productID); stockAmount < quantity {
+        return false, accountBalance
+    } else if orderTotal := calcPrice(productID, quantity); orderTotal > accountBalance {
+        return false, accountBalance
+    } else {
+        return true, accountBalance - orderTotal
+    }
+}
 
-func main() {
-    numMessagesFromDoris := 72
-    costPerMessage := .02
-    totalCost := costPerMessage * float64(numMessagesFromDoris)
-    fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+func calcPrice(productID string, quantity int) float64 {
+    return priceList(productID) * float64(quantity)
+}
+
+func priceList(productID string) float64 {
+    if productID == "1" {
+        return 1.50
+    } else if productID == "2" {
+        return 2.25
+    } else if productID == "3" {
+        return 3.00
+    } else if productID == "4" {
+        return 1.00
+    } else if productID == "5" {
+        return 2.50
+    } else if productID == "6" {
+        return 8.99
+    } else if productID == "7" {
+        return 22.50
+    } else if productID == "8" {
+        return 50.00
+    } else if productID == "9" {
+        return 999.99
+    } else {
+        return 0.00
+    }
+}
+
+func amountInStock(productID string) int {
+    if productID == "1" {
+        return 11
+    } else if productID == "2" {
+        return 25
+    } else if productID == "3" {
+        return 4
+    } else if productID == "4" {
+        return 6
+    } else if productID == "5" {
+        return 50
+    } else if productID == "6" {
+        return 2
+    } else if productID == "7" {
+        return 0
+    } else if productID == "8" {
+        return 99
+    } else if productID == "9" {
+        return 1
+    } else {
+        return 0
+    }
 }
